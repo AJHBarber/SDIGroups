b89ade91f09cfabd6e1356c2e975c2faf6e389ab
Alex <n0682736@ntu.ac.uk>
2018-04-10 23:34:42 +0100

    functioning linked list

--- CODE/SDI Project/SDI Project/LinkedList.cpp
+++ CODE/SDI Project/SDI Project/LinkedList.cpp
@@ -1,1 +1,93 @@
-//linked list funcions
+//linked list funcions need to adapt for projects
+
+#include "stdafx.h"
+#include "LinkedList.h"
+#include <iostream>
+
+
+List::~List(void) {
+	Node* currNode = head, *nextNode = NULL;
+	while (currNode != NULL) {
+		nextNode = currNode->next;
+		delete currNode;
+		currNode = nextNode;
+	}
+}
+
+Node* List::InsertNode(int index, double x) {
+
+	if (index < 0) {
+		return NULL;
+	}
+
+	int currIndex = 1;
+	Node* currNode = head;
+	while (currNode && index > currIndex) {
+		currNode = currNode->next;
+		currIndex++;
+	}
+	if (index > 0 && currNode == NULL) {
+		return NULL;
+	}
+
+	Node* newNode = new Node;
+	newNode->data = x;
+
+	if (index == 0) {
+		newNode->next = head;
+		head = newNode;
+	}
+	else {
+		newNode->next = currNode->next;
+		currNode->next = newNode;
+	}
+
+}
+
+
+int List::FindNode(double x) {
+	Node* currNode = head;
+	int currIndex = 1;
+	while (currNode && currNode->data != x) {
+		currNode = currNode->next;
+		currIndex++;
+	}
+	if (currNode) {
+		return currIndex;
+	}
+	return 0;
+}
+
+int List::DeleteNode(double x) {
+	Node* prevNode = NULL;
+	Node* currNode = head;
+	int currIndex = 1;
+	while (currNode && currNode->data != x) {
+		prevNode = currNode;
+		currNode = currNode->next;
+	}
+	if (currNode) {
+		if (prevNode) {
+			prevNode->next = currNode->next;
+			delete currNode;
+		}
+		else {
+			head = currNode->next;
+			delete currNode;
+		}
+		return currIndex;
+	}
+	return 0;
+}
+
+void List::DisplaList() {
+	int num = 0;
+	Node* currNode = head;
+	while (currNode != NULL) {
+		std::cout << currNode->data << std::endl;
+		currNode = currNode->next;
+		num++;
+	}
+	std::cout << "Number of nodes in the list: " << num << std::endl;
+
+}
--- CODE/SDI Project/SDI Project/LinkedList.h
+++ CODE/SDI Project/SDI Project/LinkedList.h
@@ -1,2 +1,29 @@
 #pragma once
-//header for linked list
+//header for linked list need to adapt for projects/ possable add stack for items like box office revenue
+#ifndef _LIST_H
+#define _LIST_H
+
+#include <iostream>
+
+class Node {
+public:
+	double data;
+	Node* next;
+
+};
+
+class List {
+public:
+	List(void) { head = NULL; }			//constructor
+	~List(void);						//destructor
+	bool IsEmpty() { return head == NULL; }
+	Node* InsertNode(int index, double x);
+	int FindNode(double x);
+	int DeleteNode(double x);
+	void DisplaList(void);
+
+private:
+	Node * head;
+};
+
+#endif 
